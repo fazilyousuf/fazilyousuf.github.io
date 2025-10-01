e841dc8f0855597ffce45ba4d790f36e4d22ae61
Fazil <fazifazu01@gmail.com>
2025-10-01 13:22:44 +0400

    minor change

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/fazilyousuf.github.io/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
