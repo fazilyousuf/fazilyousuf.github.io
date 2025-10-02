f8a8398bd9d845db45c6f13f13221e97a6cdb437
Fazil <fazifazu01@gmail.com>
2025-10-02 10:58:27 +0400

    added analytics insight

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom'; // <- import BrowserRouter
 import './index.css';
 import App from './App.jsx';
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/"> 
       <App />
       <SpeedInsights />
+      <Analytics />  
     </BrowserRouter>
   </StrictMode>
 );
